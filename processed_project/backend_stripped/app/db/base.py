6d5e0ad3cf78949fe7406f56d4dcf6075542083c
Ryan Serra <ryan_serra@hotmail.com>
2025-03-17 11:15:17 -0400

    Remove processed_project from main branch

--- processed_project/backend_stripped/app/db/base.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-from app.db.base_class import Base
-from app.models.user import Company, User, UserRole
-from app.models.associations import product_fitment_association, product_media_association, product_tariff_code_association, product_unspsc_association, product_country_origin_association, product_hardware_association, product_interchange_association, product_packaging_association, product_color_association, product_construction_type_association, product_texture_association
-from app.models.location import Address, Country
-from app.models.reference import Color, ConstructionType, Hardware, PackagingType, TariffCode, Texture, UnspscCode, Warehouse
-from app.models.product import AttributeDefinition, Brand, Fitment, Manufacturer, PriceType, Product, ProductActivity, ProductAttribute, ProductBrandHistory, ProductDescription, ProductMarketing, ProductMeasurement, ProductPricing, ProductStock, ProductSupersession
-from app.models.media import Media, MediaType, MediaVisibility
-from app.models.compliance import Prop65Chemical, ProductChemical
--- processed_project/backend_stripped/app/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from app.models.product import Fitment, Product, product_fitment_association
-from app.models.user import Company, User, UserRole
-from app.models.media import Media, MediaType, MediaVisibility, product_media_association

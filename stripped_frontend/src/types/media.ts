277ab97fa37cb6e7075c1472d8b6aa2e1c95a10f
Ryan Serra <ryan_serra@hotmail.com>
2025-03-17 11:00:44 -0400

    Fixed process_structure github actions

--- stripped_frontend/src/types/media.ts
+++
@@ -1,1 +0,0 @@
-export interface Media{id:string;filename:string;url:string;thumbnail_url?:string;mime_type:string;size:number;media_type:string;alt_text?:string;description?:string;product?:any;created_at:string;updated_at:string;}

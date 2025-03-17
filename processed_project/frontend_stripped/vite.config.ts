d15a3dde8680a01c0c8883e9c3b7a23d26016104
Ryan Serra <ryan_serra@hotmail.com>
2025-03-17 10:46:24 -0400

    Stop tracking generated files

--- processed_project/frontend_stripped/vite.config.ts
+++
@@ -1,7 +0,0 @@
-import{defineConfig}from 'vite' import vue from '@vitejs/plugin-vue' import vueJsx from '@vitejs/plugin-vue-jsx' import{fileURLToPath,URL}from 'node:url' export default defineConfig({plugins:[vue(),vueJsx()],resolve:{alias:{'@':fileURLToPath(new URL('./src',import.meta.url))}},server:{proxy:{'/api':{target:'http:
-        changeOrigin: true,
-      }
-    }
-  },
-  test: {
-    environment: 'jsdom',globals:true}})
